4343ac5bbe792cf10a97cac4c59e6ccb516bb64e
AdrianRMV <adrianthelawx@gmail.com>
2024-02-25 19:41:20 -0700

    First Commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+import React from 'react';
+import { ThemeToggle } from './ThemeToggle';
+import { Button } from '@/components/ui/button';
+import {
+    RegisterLink,
+    LoginLink,
+    LogoutLink,
+} from '@kinde-oss/kinde-auth-nextjs/components';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+
+const Navbar = async () => {
+    const { isAuthenticated } = getKindeServerSession();
+    return (
+        <nav className="border-b bg-background h-[10vh] flex items-center">
+            <div className="container flex items-center justify-between">
+                <Link href="/">
+                    <h1 className="font-bold text-3xl">ARV SAAS</h1>
+                </Link>
+
+                <div className="flex items-center gap-x-5">
+                    <ThemeToggle />
+
+                    {(await isAuthenticated) ? (
+                        <div className="flex items-center gap-x-5">
+                            <LogoutLink>
+                                <Button>Log Out</Button>
+                            </LogoutLink>
+                        </div>
+                    ) : (
+                        <div className="flex items-center gap-x-5">
+                            <LoginLink>
+                                <Button>Sign In</Button>
+                            </LoginLink>
+                            <RegisterLink>
+                                <Button variant="secondary">Sign Up</Button>
+                            </RegisterLink>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+    return (
+        <div>
+            <h1>Hello from dashboard</h1>
+        </div>
+    );
+};
+
+export default DashboardPage;
