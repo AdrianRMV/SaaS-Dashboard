deda05ef5dc9dda9c19060808f3b40afe1e162c9
AdrianRMV <adrianthelawx@gmail.com>
2024-02-28 23:52:32 -0700

    User to DB done

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,58 @@
 import { ReactNode } from 'react';
 import DashboardNav from '../components/DashboardNav';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { redirect } from 'next/navigation';
+import prisma from '../lib/db';
+const getData = async ({
+    email,
+    id,
+    firstName,
+    lastName,
+    profileImage,
+}: {
+    email: string;
+    id: string;
+    firstName: string | undefined | null;
+    lastName: string | undefined | null;
+    profileImage: string | undefined | null;
+}) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: id,
+        },
+        select: {
+            id: true,
+            stripeCustomerId: true,
+        },
+    });
 
-function DasboardLayout({ children }: { children: ReactNode }) {
+    if (!user) {
+        // Juntamos nombre con apellido ya que en la db solo tenemos un campo para el nombre en general y no en especifo por apellidos
+        const name = `${firstName ?? ''} ${lastName ?? ''}`;
+        await prisma.user.create({
+            data: {
+                id: id,
+                email: email,
+                name: name,
+            },
+        });
+    }
+};
+
+async function DasboardLayout({ children }: { children: ReactNode }) {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user) {
+        return redirect('/');
+    }
+    await getData({
+        email: user.email as string,
+        firstName: user.given_name as string,
+        id: user.id,
+        lastName: user.family_name as string,
+        profileImage: user.picture,
+    });
     return (
         <div className="flex flex-col space-y-6 mt-10">
             <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
