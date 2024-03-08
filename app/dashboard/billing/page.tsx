a98b2c40d7683360f98539637e5a7364192c82f7
AdrianRMV <adrianthelawx@gmail.com>
2024-03-08 13:51:00 -0700

    More details for production

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -68,7 +68,10 @@ const BillingPage = async () => {
 
         const subscriptionUrl = await getStripeSession({
             customerId: dbUser.stripeCustomerId,
-            domainUrl: 'http://localhost:3000',
+            domainUrl:
+                process.env.NODE_ENV === 'production'
+                    ? (process.env.PRODUCTION_URL as string)
+                    : 'http://localhost:3000',
             priceId: process.env.STRIPE_PRICE_ID as string,
         });
 
